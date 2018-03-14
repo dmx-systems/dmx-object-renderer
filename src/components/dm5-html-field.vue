c23b6d084aab1b9b547f6b8ffecb3b270c6b82cb
Jörg Richter <jri@deepamehta.de>
2018-03-14 21:57:10 +0100

    Composability: host app can provide Quill extensions

--- src/components/dm5-html-field.vue
+++ src/components/dm5-html-field.vue
@@ -3,7 +3,8 @@
   <div v-else>
     <!-- Without this wrapper <div> the Quill toolbar remains visible when switching to info mode.   -->
     <!-- This is because the Quill toolbar becomes a *sibling* (not a child) of the <quill> element. -->
-    <quill v-model="object.value" :options="quillOptions" @quill-imported="registerExtension" @quill-ready="quillReady">
+    <quill v-model="object.value" :options="quillOptions" @quill-imported="registerExtensions"
+      @quill-ready="quillReady">
     </quill>
   </div>
 </template>
@@ -18,12 +19,12 @@ export default {
   mounted () {
     // console.log('dm5-html-field mounted()', this.mode, this.$refs.html)
     // Note: if a topic is edited for the first time the dm5-html-field component is mounted in "form" mode
-    this.addLinkHandlers()
+    this.infoDOMReady()
   },
 
   updated () {
     // console.log('dm5-html-field updated()', this.mode)
-    this.addLinkHandlers()
+    this.infoDOMReady()
   },
 
   mixins: [
@@ -34,80 +35,43 @@ export default {
   ],
 
   computed: {
+
     quillOptions () {
       return this.context.quillConfig.options
+    },
+
+    quillExtensions () {
+      return this.context.quillConfig.extensions
     }
   },
 
   methods: {
 
-    // TODO: move the entire "topic link" aspect to the host app
-
-    // TODO: let the host app register extensions
-    registerExtension (Quill) {
-      Quill.register(topicLinkFormat(Quill))
+    registerExtensions (Quill) {
+      this.forEachExtension(ext => Quill.register(ext.extension(Quill)))
     },
 
-    // TODO: let the host app setup the format buttons
     quillReady (quill) {
-      // FIXME: handle multiple Quill instances
-      const button = document.querySelector('button.ql-topic-link')
-      button.textContent = 'T'    // TODO: use real (SVG) icon
-      button.setAttribute('title', 'Embed a topic link')
+      this.forEachExtension(ext => {
+        quill.getModule('toolbar').addHandler(ext.name, ext.toolbarHandler)
+        ext.quillReady(quill)
+      })
     },
 
-    // TODO: let the host app setup the link handlers
-    addLinkHandlers () {
+    infoDOMReady () {
       if (this.infoMode) {
-        // FIXME: event handlers are registered twice, through updated()
-        this.$refs.html.querySelectorAll('a.topic-link').forEach(link => {
-          link.addEventListener('click', e => {
-            const topicId = Number(e.target.dataset.topicId)
-            console.log('topic link clicked', topicId)
-            this.$store.dispatch('revealTopicById', topicId)
-            e.preventDefault()    // suppress browser's default link click behavior
-            e.stopPropagation()   // prevent activating inline edit
-          })
-        })
+        this.forEachExtension(ext => ext.infoDOMReady(this.$refs.html))
       }
+    },
+
+    forEachExtension (visitor) {
+      this.quillExtensions && this.quillExtensions.forEach(ext => visitor(ext))
     }
   },
 
   components: {
     quill: () => import(/* webpackChunkName: "quill" */ 'vue-quill-minimum')
   }
-}
-
-function topicLinkFormat (Quill) {
-
-  class TopicLink extends Quill.import('formats/link') {    // Note: 'formats/link' extends 'blots/inline'
-
-    // Creates a DOM node corresponding to the given (model) values
-    static create (value) {
-      // console.log('TopicLink create()', value)
-      const node = super.create()
-      node.removeAttribute('target')    // target attribute was added by Link class
-      node.setAttribute('href', '')     // href attribute required to render in link style
-      node.dataset.topicId = value.topicId
-      node.dataset.linkId  = value.linkId
-      return node
-    }
-
-    // Returns the (model) value represented by the given DOM node
-    static formats (node) {
-      // console.log('TopicLink formats()', node)
-      return {
-        topicId: node.dataset.topicId,  // FIXME: convert to Number?
-        linkId:  node.dataset.linkId    // FIXME: convert to Number?
-      }
-    }
-  }
-
-  TopicLink.blotName = 'topic-link'     // Used in "toolbar" config, quill.format() and module.addHandler() calls.
-                                        // Will be prefixed by "ql-" to form the CSS class name for the toolbar button.
-  // TopicLink.tagName = 'A'            // not needed as it is derived
-  TopicLink.className = 'topic-link'    // CSS class to be added to the <a> element
-  return TopicLink
 }
 </script>
 
