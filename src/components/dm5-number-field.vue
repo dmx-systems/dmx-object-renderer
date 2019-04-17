eaeecc3556657e910a58ed1aa715123826940413
Jörg Richter <jri@deepamehta.de>
2019-04-17 13:24:01 +0200

    Fix: serialize/store/retrieve values of Number fields
    See dmx-platform/dmx-platform#179

--- src/components/dm5-number-field.vue
+++ src/components/dm5-number-field.vue
@@ -1,14 +1,32 @@
 <template>
   <div v-if="infoMode">{{object.value}}</div>
-  <el-input v-else v-model="object.value" type="number"></el-input>
+  <el-input v-else v-model="value" type="number"></el-input>
 </template>
 
 <script>
 export default {
+
   mixins: [
     require('./mixins/object').default,
     require('./mixins/mode').default,
     require('./mixins/info-mode').default
-  ]
+  ],
+
+  computed: {
+    value: {
+      get () {
+        return this.object.value
+      },
+      set (value) {
+        let val
+        if (value === '') {
+          val = ''
+        } else {
+          val = Number(value)
+        }
+        this.object.value = val
+      }
+    }
+  }
 }
 </script>
