f1d48efffa2af2ae1fccc0fc16e41ebb732c51ee
Jörg Richter <jri@deepamehta.de>
2019-05-04 20:25:51 +0200

    Refactor HTML-field template
    See dmx-platform/dmx-platform#198

--- src/components/dm5-html-field.vue
+++ src/components/dm5-html-field.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="dm5-html-field" v-if="infoMode" v-html="object.value" ref="info"></div>
-  <div class="dm5-html-field" v-else>
-    <!-- Without this wrapper <div> the Quill toolbar remains visible when switching to info mode.   -->
-    <!-- This is because the Quill toolbar becomes a *sibling* (not a child) of the <quill> element. -->
-    <quill v-model="object.value" :options="quillOptions" @quill-imported="registerExtensions"
-      @quill-ready="quillReady">
-    </quill>
-  </div>
+  <quill class="dm5-html-field" v-else v-model="object.value" :options="quillOptions" ref="quill"
+    @quill-imported="registerExtensions" @quill-ready="quillReady">
+  </quill>
 </template>
 
 <script>
