d5815a3dfcafe48dbf646a7cb46200c3ce1ab792
Jörg Richter <jri@deepamehta.de>
2019-08-15 18:30:06 +0200

    Use revised query strategy for auto-complete fields
    See dmx-platform/dmx-platform#287

--- src/components/dm5-text-field.vue
+++ src/components/dm5-text-field.vue
@@ -16,8 +16,8 @@ export default {
   ],
 
   computed: {
-    searchQuery () {
-      return this.object.value + '*'    // TODO: adapt to dm5-search-widget logic
+    query () {
+      return dm5.utils.fulltextQuery(this.object.value)
     }
   },
 
@@ -27,16 +27,24 @@ export default {
       // console.log('fetchSuggestions', value)
       // Note: for an empty value fetchSuggestions() is not invoked
       // Note: fetchSuggestions() is already debounced by Element UI
-      dm5.restClient.queryTopicsFulltext(this.searchQuery, this.object.typeUri).then(queryResult => {
-        cb(queryResult.topics)
-      })
+      // compare to dm5-search-widget.vue (module dm5-search-widget)
+      console.log('query', this.query)
+      if (this.query) {
+        dm5.restClient.queryTopicsFulltext(this.query, this.object.typeUri).then(result => {
+          if (result.query === this.query) {
+            cb(result.topics)
+          } else {
+            console.log(`Ignoring ${result.topics.length} result topics of query "${result.query}"` +
+              ` (current query is "${this.query}")`)
+          }
+        }).catch(e => {
+          console.warn(`Query "${this.query}" failed`)
+          cb([])
+        })
+      } else {
+        cb([])
+      }
     }
-
-    /* enter (e) {
-      // ### TODO: submit only on "enter", not on "select"
-      const item = document.querySelector('ul.el-autocomplete-suggestion__list > li')  // > li.highlighted
-      console.log('enter', e, item)
-    } */
   }
 }
 </script>
