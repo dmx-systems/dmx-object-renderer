0099a639156334bf4959281562f412c579851a8f
Jörg Richter <jri@deepamehta.de>
2019-08-17 23:26:06 +0200

    Fix: show proper value for "Select" fields
    See dmx-platform/dmx-platform#288

--- src/components/dm5-select-field.vue
+++ src/components/dm5-select-field.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="infoMode">{{displayValue}}</div>
+  <div v-if="infoMode">{{object.value}}</div>
   <el-select v-else v-model="selection" :clearable="clearable" :allow-create="customizable" :filterable="customizable"
       placeholder="" default-first-option>
     <el-option v-for="topic in sortedOptions" :label="topic.value" :value="topic.id" :key="topic.id"></el-option>
@@ -21,22 +21,40 @@ export default {
   data () {
     // console.log('data', this._uid, this.mode, this.selection, typeof this.selection)
     return {
-      // Buffered object value to be displayed in info mode.
-      // With inline editing the object is sent to the server in info mode.
-      // The update request must contain the refs, but the refs must not be displayed.
-      // The original object value is overridden with a ref (see syncObjectValue()).
-      displayValue: this.object.value,
-      // Select model
-      // ID of option topic, or text entered (if <el-select> is customizable).
-      // Note: since Element UI 2.7.1 a <el-select> represents "no selection" by null value (instead '').
-      // https://github.com/ElemeFE/element/issues/14322
-      selection: this.object.id === -1 ? null : this.object.id,
       // Option topics (object, mapped by topic ID), fetched asynchronously
       options: undefined
     }
   },
 
   computed: {
+
+    // Selection model
+    //
+    // ID of option topic, or text entered (if <el-select> is customizable).
+    // Note: since Element UI 2.7.1 a <el-select> represents "no selection" by null value (instead '').
+    // Since Element UI 2.9.0 this change is reverted; "no selection" is represented by empty string again.
+    // https://github.com/ElemeFE/element/issues/14322
+    selection: {
+      get () {
+        // console.log('get selection', this._uid, this.mode, this.object.id, this.object.value)
+        return this.object.id === -1 ? this.object.value : this.object.id
+      },
+      set (selection) {
+        // console.log('set selection', this._uid, this.mode, selection, typeof selection)
+        if (typeof selection === 'number') {
+          // Case 1: existing option selected
+          this.object.value = this.options[selection].value
+          this.object.id = selection
+        } else {
+          // Case 2: custom value entered (selection is of type string) or cleared (selection is null)
+          this.object.value = selection
+          this.object.id = -1
+          // Note: custom values get no URI. An existing URI must be reset.
+          // Otherwise an URI clash might occur at server side while creating the custom topic.
+          this.object.uri = ''
+        }
+      }
+    },
 
     sortedOptions () {
       return this.options && Object.values(this.options).sort(
@@ -55,86 +73,31 @@ export default {
     }
   },
 
-  // In form mode a) the option topics must be loaded, and b) the object value must be synced.
+  // In form mode the option topics must be loaded.
   // 2 cases:
   //   1) This component is created in info mode. When switched to form mode (global edit or inline edit)
   //      the mode watcher fires.
   //   2) This component is created in form mode (global edit).
   //      The mode watcher does *not* fire.
-  // So we must call loadOptions() and syncObjectValue() in both places, the created() hook and the mode watcher.
+  // So we must call loadOptions() in both places, the created() hook and the mode watcher.
   created () {
     // console.log('created', this._uid, this.mode, this.selection, typeof this.selection)
-    if (this.formMode) {
-      this.loadOptions()
-      this.syncObjectValue()
-    }
+    this.loadOptions()
   },
 
   watch: {
-
     mode () {
       // console.log('mode', this._uid, this.mode, this.selection, typeof this.selection)
-      if (this.formMode) {
-        this.loadOptions()
-        this.syncObjectValue()
-      }
-    },
-
-    selection () {
-      // console.log('selection', this._uid, this.mode, this.selection, typeof this.selection)
-      this.syncObjectValue()
-      this.syncDisplayValue()
+      this.loadOptions()
     }
   },
 
   methods: {
-
     loadOptions () {
-      if (!this.options) {
+      if (this.formMode && !this.options) {
         dm5.restClient.getTopicsByType(this.object.typeUri).then(topics => {
           this.options = dm5.utils.mapById(topics)
         })
-      }
-    },
-
-    /**
-     * Syncs object value to selection.
-     */
-    syncObjectValue () {
-      this.checkSelection()
-      if (typeof this.selection === 'number') {
-        // Case 1: existing option selected
-        this.object.value = `ref_id:${this.selection}`
-      } else {
-        // Case 2: custom value entered (selection is of type string) or cleared (selection is null)
-        // Note: a serialized object sent to the server must not contain JSON null, but ''
-        this.object.value = this.selection || ''
-        // Note: custom values get no URI. An existing URI must be reset.
-        // Otherwise an URI clash might occur at server side while creating the custom topic.
-        this.object.uri = ''
-      }
-    },
-
-    /**
-     * Syncs display value to selection.
-     */
-    syncDisplayValue () {
-      this.checkSelection()
-      if (typeof this.selection === 'number') {
-        // Case 1: existing option selected
-        this.displayValue = this.options[this.selection].value
-      } else {
-        // Case 2: custom value entered (selection is of type string) or cleared (selection is null)
-        this.displayValue = this.selection || ''
-      }
-    },
-
-    // sanity check
-    checkSelection () {
-      // Note: since Element UI 2.7.1 a cleared <el-select> has null value (instead '')
-      // https://github.com/ElemeFE/element/issues/14322
-      if (this.selection !== null && !['number', 'string'].includes(typeof this.selection)) {
-        throw Error(`unexpected selection: ${this.selection} ${typeof this.selection}`)
       }
     }
   }
