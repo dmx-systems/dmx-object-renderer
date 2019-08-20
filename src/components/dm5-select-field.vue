46ae3507826b7b7f2da4d60a0aaafa21ab925376
Jörg Richter <jri@deepamehta.de>
2019-08-20 21:24:14 +0200

    Fix: cardinality many "Select" fields
    See dmx-platform/dmx-platform#288

--- src/components/dm5-select-field.vue
+++ src/components/dm5-select-field.vue
@@ -1,8 +1,16 @@
 <template>
   <div v-if="infoMode">{{object.value}}</div>
-  <el-select v-else v-model="selection" :clearable="clearable" :allow-create="customizable" :filterable="customizable"
-      placeholder="" default-first-option>
-    <el-option v-for="topic in sortedOptions" :label="topic.value" :value="topic.id" :key="topic.id"></el-option>
+  <!--
+    Note: since Element UI 2.7.1 an el-select represents "no selection" by null value (instead '').
+    Since Element UI 2.9.0 this change is reverted; "no selection" is represented by '' again.
+    https://github.com/ElemeFE/element/issues/14322
+    So for the selection model we can directly use the object value; no conversion required. Furthermore we
+    don't need any by-ID refs in the update request but can solely rely on the server's value integration.
+  -->
+  <el-select v-else v-model="object.value" :clearable="clearable" :allow-create="customizable"
+      :filterable="customizable" placeholder="" default-first-option @change="change">
+    <el-option v-for="topic in sortedOptions" :label="topic.value" :value="topic.value" :key="topic.id">
+    </el-option>
   </el-select>
 </template>
 
@@ -19,7 +27,6 @@ export default {
   ],
 
   data () {
-    // console.log('data', this._uid, this.mode, this.selection, typeof this.selection)
     return {
       // Option topics (object, mapped by topic ID), fetched asynchronously
       options: undefined
@@ -27,34 +34,6 @@ export default {
   },
 
   computed: {
-
-    // Selection model
-    //
-    // ID of option topic, or text entered (if <el-select> is customizable).
-    // Note: since Element UI 2.7.1 a <el-select> represents "no selection" by null value (instead '').
-    // Since Element UI 2.9.0 this change is reverted; "no selection" is represented by empty string again.
-    // https://github.com/ElemeFE/element/issues/14322
-    selection: {
-      get () {
-        // console.log('get selection', this._uid, this.mode, this.object.id, this.object.value)
-        return this.object.id === -1 ? this.object.value : this.object.id
-      },
-      set (selection) {
-        // console.log('set selection', this._uid, this.mode, selection, typeof selection)
-        if (typeof selection === 'number') {
-          // Case 1: existing option selected
-          this.object.value = this.options[selection].value
-          this.object.id = selection
-        } else {
-          // Case 2: custom value entered (selection is of type string) or cleared (selection is null)
-          this.object.value = selection
-          this.object.id = -1
-          // Note: custom values get no URI. An existing URI must be reset.
-          // Otherwise an URI clash might occur at server side while creating the custom topic.
-          this.object.uri = ''
-        }
-      }
-    },
 
     sortedOptions () {
       return this.options && Object.values(this.options).sort(
@@ -81,24 +60,29 @@ export default {
   //      The mode watcher does *not* fire.
   // So we must call loadOptions() in both places, the created() hook and the mode watcher.
   created () {
-    // console.log('created', this._uid, this.mode, this.selection, typeof this.selection)
     this.loadOptions()
   },
 
   watch: {
     mode () {
-      // console.log('mode', this._uid, this.mode, this.selection, typeof this.selection)
       this.loadOptions()
     }
   },
 
   methods: {
+
     loadOptions () {
       if (this.formMode && !this.options) {
         dm5.restClient.getTopicsByType(this.object.typeUri).then(topics => {
           this.options = dm5.utils.mapById(topics)
         })
       }
+    },
+
+    change () {
+      // Note: custom values get no URI. An existing URI must be reset.
+      // Otherwise an URI clash might occur at server side while creating the custom topic.
+      this.object.uri = ''
     }
   }
 }
